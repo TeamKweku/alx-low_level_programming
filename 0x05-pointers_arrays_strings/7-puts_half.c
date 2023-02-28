76fcacb615fe16d3e96bb8af388ef7353aae85b2
Theophilus Ackom <teamkweku@outlook.com>
2023-02-28 00:52:29 +0000

    function that prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,9 +15,14 @@ void puts_half(char *str)
 	while (*(str + len) != '\0')
 		len++;
 	if (len % 2 != 0)
+	{
 		i = (len - 1) / 2;
+		i += 1;
+	}
 	else
+	{
 		i = len / 2;
+	}
 	for (; i < len; i++)
 	{
 		_putchar(*(str + i));
