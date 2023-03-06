043c5faf1a0c22bda487cf029cdefa3e600aa976
Theophilus Ackom <teamkweku@outlook.com>
2023-03-06 00:37:51 +0000

    sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
+
