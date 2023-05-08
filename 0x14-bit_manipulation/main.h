5cab409cdaf994f52f7a146e4d8f05b2e5277ecf
Theophilus Ackom <teamkweku@outlook.com>
2023-05-08 12:45:08 +0000

    function that reads a text file and prints it to the POSIX standard output.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+<<<<<<< HEAD
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+=======
+
+>>>>>>> parent of c7332c7... function that prints the binary representation of a number
 
 #endif
